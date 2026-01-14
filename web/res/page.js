265cdf2aa6062e3c8e6c36b089596b93dc3885bd
Axel-UiS <axsel2381@uis.no>
2026-01-14 08:15:42 +0100

    Web2.0 - Added nav#leftnavbar content, changed some UI-flow, moved logo to top.

--- web/res/page.js
+++ web/res/page.js
@@ -1,15 +1,53 @@
 let topnavbar = document.querySelector("nav#topnavbar");
+let leftnavbar = document.querySelector("nav#leftnavbar");
+
+function openproject(event) {
+    if (leftnavbar != null) {
+        let elem = leftnavbar.querySelector("#project");
+        if (elem.style.display != "none") {
+            elem.style.display = "none";
+        } else {
+            elem.style.display = "block";
+        }
+    }
+};
+
+function opencodebase(event) {
+    if (leftnavbar != null) {
+        let elem = leftnavbar.querySelector("#codebase");
+        if (elem.style.display != "none") {
+            elem.style.display = "none";
+        } else {
+            elem.style.display = "block";
+        }
+    }
+};
+
+function opencodefi(event) {
+    if (leftnavbar != null) {
+        let elem = leftnavbar.querySelector("#codefi");
+        if (elem.style.display != "none") {
+            elem.style.display = "none";
+        } else {
+            elem.style.display = "block";
+        }
+    }
+};
 
 function topnavbarSymbolsOrText(event) {
     if (topnavbar != null) {
         let symbols = topnavbar.getElementsByClassName("symbol");
         let text = topnavbar.getElementsByClassName("text");
-        if (window.innerWidth < 768) {
+        let logo = topnavbar.getElementsByClassName("logo");
+        if (window.innerWidth < 1008) {
             for (var i = 0; i < symbols.length; i++) {
                 symbols[i].style.display = "block";
             }
             for (var i = 0; i < text.length; i++) {
                 text[i].style.display = "none";
+            }
+            for (var i = 0; i < logo.length; i++) {
+                logo[i].style.display = "none";
             }
         } else {
             for (var i = 0; i < symbols.length; i++) {
@@ -18,10 +56,12 @@ function topnavbarSymbolsOrText(event) {
             for (var i = 0; i < text.length; i++) {
                 text[i].style.display = "block";
             };
+            for (var i = 0; i < logo.length; i++) {
+                logo[i].style.display = "inline-block";
+            }
         };
     };
 };
 
 window.onresize = topnavbarSymbolsOrText;
-
 topnavbarSymbolsOrText();
